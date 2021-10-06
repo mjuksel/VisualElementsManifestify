fc9456f23423023846f1386a61b9487bbbe09774
Mjuksel <mportma@gmail.com>
2021-10-06 04:55:56 +0200

    Update index.js
    added arguments for on the command line.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-require('fs').readdir('.', 'utf8', (error, files) => {
+const fs = require('fs')
+const process = require('process')
+const Arg = process.argv.filter((v,i) => i >= 2 && v)
+
+fs.readdir('.', 'utf8', (error, files) => {
 	if (error) throw new Error(error)
 	for (const file of files) {
 		if (file.startsWith('icon')) {
-			let applicationName = file.replace(/(icon|\.(png|jpg))/g, ''),
-				backgroundColor = '#123123',
-				textLightDark = 'dark',
-				showNameOnOff = 'off'
-			fs.writeFileSync(
-				`${applicationName}.VisualElementsManifest.xml`,
-				`
+			let applicationName = file.replace(/icon([\w|\d]+)\.[png|jpg])/g, '')
+			let backgroundColor = Arg[0] || '#123123'
+			let textLightDark = Arg[1] || 'dark'
+			let showNameOnOff = Arg[2] || 'off'
+			fs.writeFileSync(`${applicationName}.VisualElementsManifest.xml`,`
 <Application xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 <VisualElements
     BackgroundColor="${backgroundColor}"
