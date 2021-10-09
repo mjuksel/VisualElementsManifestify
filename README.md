483c535f3eb48f59db8bbfcd515ceadb2cbaec7c
Mjuksel <mportma@gmail.com>
2021-10-09 20:47:26 +0200

    y0

--- README.md
+++ README.md
@@ -6,16 +6,18 @@ At some point a new version of the Start Menu appeared, development on OblyTile 
 
 Some other apps also did pinning, like pinning Steam Games and Websites, among others.
 
-After a bit I created this NodeJS script to create VisualElementsManifest.xml by checking if the folder contains a file named icon`*name of app/or whatever else*.[png/jpg]`.
+After a bit I created this NodeJS script to create VisualElementsManifest.xml by checking if the folder contains a file named `icon[*ANYTHING*.png/jpg]`.
 
 If it does, create an XML file, pointing to the icon file, and some other options.
 
 # Usage
 
-In a terminal you can pass arguments in order: Color, Dark/Light Text, Show Name on the tile.
+`node x:\VSM\index.js` will create the Manifest with predefined values in `index.js`.
+
+In a terminal you can pass arguments:
+ - '#colorr' (Color in HEX6)
+ - 'dark/light' (Text on tile)
+ - 'on/off' (Name on tile)
 
 Example (from commandline):
 `node x:\VSM\index.js '#0099FF' 'light' 'on'`
-
-There are default values in the script, you can change those and then just use `node x:\VSM\index.js`.
-
--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const fs = require('fs')
 const process = require('process')
-const Arg = process.argv.filter((v,i) => i >= 2 && v)
+const args = process.argv.filter((v, i) => i >= 2 && v)
 
 fs.readdir('.', 'utf8', (error, files) => {
 	if (error) throw new Error(error)
 	for (const file of files) {
 		if (file.startsWith('icon')) {
-			let applicationName = file.replace(/icon([\w|\d]+)\.[png|jpg])/g, '')
-			let backgroundColor = Arg[0] || '#123123'
-			let textLightDark = Arg[1] || 'dark'
-			let showNameOnOff = Arg[2] || 'off'
-			fs.writeFileSync(`${applicationName}.VisualElementsManifest.xml`,`
-<Application xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-<VisualElements
-    BackgroundColor="${backgroundColor}"
-    ShowNameOnSquare150x150Logo="${showNameOnOff}"
-    Square150x150Logo="${file}"
-    Square70x70Logo="${file}"
-    ForegroundText="${textLightDark}" />
-</Application>
-`
+			let applicationName = file.replace(/icon(\w|\d+)\.png|jpg/g, '')
+			let backgroundColor = args[0] || '#123123'
+			let textLightDark = args[1] || 'dark'
+			let showNameOnOff = args[2] || 'off'
+			fs.writeFileSync(
+				`${applicationName}.VisualElementsManifest.xml`,
+				`<Application xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+			<VisualElements
+			    BackgroundColor="${backgroundColor}"
+			    ShowNameOnSquare150x150Logo="${showNameOnOff}"
+			    Square150x150Logo="${file}"
+			    Square70x70Logo="${file}"
+			    ForegroundText="${textLightDark}" />
+			</Application>
+			`
 			)
 		}
 	}
